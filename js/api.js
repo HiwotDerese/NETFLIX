6000643b7e9a2477a9e78ab4e6c6b03fb956a0f6
Hiwot Derese <Hiwotderese16@gmail.com>
2022-08-16 07:43:20 +0300

    fetch images from api

---
+++ js/api.js
@@ -0,0 +1,4 @@
+const API_KEY = 'api_key=c1cd2833b1ed62c295469590abfceb40';
+const BASE_URL = 'https://api.themoviedb.org/3';
+const API_URL =  BASE_URL + '/discover/movie?sort_by=popularity.desc&' 
++ API_KEY;
--- js/index.js
+++ js/index.js
@@ -4,84 +4,154 @@ const API_KEY = 'api_key=c1cd2833b1ed62c295469590abfceb40';
 const BASE_URL = 'https://api.themoviedb.org/3';
 const API_URL =  BASE_URL + '/discover/movie?sort_by=popularity.desc&' 
 + API_KEY;
-
-
 // const API_URL =  BASE_URL + '/genre/movie/list?' + API_KEY; 
 
 const IMG_URL = 'https://image.tmdb.org/t/p/w500'; 
 
+const genres = [
+        {
+            "id":28,
+            "name":"Action"
+        },
+        {
+            "id":12,
+            "name":"Adventure"
+        },
+        {
+            "id":16,
+            "name":"Animation"
+        },
+        {
+            "id":35,
+            "name":"Comedy"
+        },
+        {
+            "id":80,
+            "name":"Crime"
+        },
+        {
+            "id":99,
+            "name":"Documentary"
+        },
+        {
+            "id":18,
+            "name":"Drama"
+        },
+        {
+            "id":10751,
+            "name":"Family"
+        },
+        {
+            "id":14,
+            "name":"Fantasy"
+        },
+        {
+            "id":36,
+            "name":"History"
+        },
+        {
+            "id":27,
+            "name":"Horror"
+        },
+        {
+            "id":10402,
+            "name":"Music"
+        },
+        {
+            "id":9648,
+            "name":"Mystery"
+        },
+        {
+            "id":10749,
+            "name":"Romance"
+        },
+        {
+            "id":878,
+            "name":"Science Fiction"
+        },
+        {
+            "id":10770,
+            "name":"TV Movie"
+        },
+        {
+            "id":53,
+            "name":"Thriller"
+        },
+        {
+            "id":10752,
+            "name":"War"
+        },
+        {
+            "id":37,
+            "name":"Western"
+        }
+    ]
+     
+
+
 const main = document.getElementById('main');
+const tagsEl = document.getElementById('tags')
+// const main2 = document.getElementById('main2');
+// const tags2El = document.getElementById('tags2');
+// let popup = document.getElementById('popup');
 
 
-getMovies(API_URL);
+function openPopup(){
+    popup.classList.add('open-popup');
+}
 
-function getMovies(url){
+function closePopup(){
+    popup.classList.remove('open-popup');
+}
 
-    fetch(url).then(res => res.json()).then(data => {
-        console.log(data.results);
-        showMovies(data.results);
+
+
+
+
+
+setGenre();
+function setGenre(){
+    genres.forEach(genre => {
+        fetch(`https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=c1cd2833b1ed62c295469590abfceb40&with_genres=12`).then(res => res.json()).then(data => {
+            console.log(data.results)
+            const box = document.createElement('div');
+            box.classList.add('genre')
+            const title = document.createElement('div');
+            title.classList.add('genre_name')
+            box.appendChild(title)
+            title.innerHTML = genre.name;
+            const images = document.createElement('div');
+            box.appendChild(images); 
+            const imgLink = document.createElement('a');
+            images.appendChild(imgLink);
+            // images.innerHTML = '';
+            // images.appendChild(img)
+            
+            
+            
+
+            data.results.forEach((id_data)=>{
+                if(id_data.genre_ids.includes(genre.id)){
+                    console.log(id_data.poster_path,genre.id);
+              
+                    const image_url=`${IMG_URL + id_data.poster_path}`
+                    console.log(image_url,"uuuuuuuuuuuuuuuuuuuuuuuuuu");
+                    const img = document.createElement('img');
+                    img.setAttribute('src', image_url);
+                    imgLink.setAttribute('id', 'box')
+                    imgLink.appendChild(img);
+                    imgLink.setAttribute('href','./html/detail-page.html')
+                    // img.setAttribute('href', './css/detail.css')
+                    // box.setAttribute('id','box');
+                    images.setAttribute('id','box');
+                    images.appendChild(imgLink);
+                    tagsEl.appendChild(box);
+                    
+                }
+            }
+            )  
+        })
     })
 }
 
-function showMovies(data){
-    main.innerHTML = '';
 
-    data.forEach(movie => {
-        //         const{poster_path} = posimg;
-        const{poster_path} = movie;
-        const movieEl = document.createElement('a');
-        movieEl.classList.add('movie');
-        movieEl.innerHTML = `
-        <img src="${IMG_URL + poster_path}" alt="img">
-        
-        `
-        movieEl.href = "./html/detail-page.html";
-        main.appendChild(movieEl);
-        
-    });
-
-}
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// const posters = document.getElementById('posters');
-
-// getMovies(API_URL);
-
-// function getMovies(url){
-//     fetch(url).then(res => res.json()).then(data =>{
-//         console.log(data.results);
-//         // showMovies(data.results);
-//     })
-// }
-
-
-// function showMovies(data){
-//     posters.innerHTML = '';
-
-//     data.forEach(posimg => {
-//         const{poster_path} = posimg;
-//         const posimgEl = document.createElement('div');
-//         posimgEl.classList.add('posimg');
-//         posimgEl.innerHTML = `<img src="${IMG_URL + poster_path}" alt="the i-land">`
-
-//         posters.appendChild()
-        
-//     })
-// }
